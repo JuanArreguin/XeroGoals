bc3554b4b16a17b97bba21cee1aa440656e941c1
Juan Arreguin <contact@juanarreguin.com>
2014-04-11 08:15:58 -0700

    Initial Push

---
+++ XeroGoals/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  Ripple
+//
+//  Created by Meng To on 29/11/13.
+//  Copyright (c) 2013 Meng To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
+@implementation UINavigationController (Rotation_IOS6)
+
+-(BOOL)shouldAutorotate
+{
+    return [[self.viewControllers lastObject] shouldAutorotate];
+}
+
+-(NSUInteger)supportedInterfaceOrientations
+{
+    return [[self.viewControllers lastObject] supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [[self.viewControllers lastObject] preferredInterfaceOrientationForPresentation];
+}
+
+@end
